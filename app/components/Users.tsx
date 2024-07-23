5bf9950f9f5f8f99661a7907bd1e482695b56d0d
jamesmaccoy <thankyou.digital@gmail.com>
2024-07-23 16:49:57 +0200

    w

--- app/components/Users.tsx
+++
@@ -1,9 +0,0 @@
-export default async function Users() {
-    return (
-      <section>
-        <div className='p-8'>
-          <h2 className='font-semibold'>Select a user to see list of todos</h2>
-        </div>
-      </section>
-    )
-  }
